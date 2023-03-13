13699f0879ee0826c7298992d8a81b7276788b0c
sutaminajing40 <sutaminajing40@gmail.com>
2023-03-13 13:43:12 +0900

    update

--- app.py
+++ app.py
@@ -66,18 +66,14 @@ def authorization():
     if not auth_manager.validate_token(cache_handler.get_cached_token()):
         auth_url = auth_manager.get_authorize_url()  # log in url
 
-        # if you're redirected from the sign in page, there is a code in the url
-        if 'code' in st.experimental_get_query_params():  
-            auth_manager.get_access_token(st.experimental_get_query_params()['code'])  # use the code to generate the token
+        if st.button('Log in'):
+            js = "window.open('{}')".format(auth_url)  # New tab or window
+            #js = "window.location.href = '{}'".format(os.environ['SPOTIPY_REDIRECT_URI'])  # Current tab
+            html = '<img src onerror="{}">'.format(js)
+            div = Div(text=html)
+            st.bokeh_chart(div) 
             sp = spotipy.Spotify(auth_manager=auth_manager)  
-        else:  # if no code, add a button linking to the log in url
-            if st.button('Log in'):
-                js = "window.open('{}')".format(auth_url)  # New tab or window
-                #js = "window.location.href = '{}'".format(os.environ['SPOTIPY_REDIRECT_URI'])  # Current tab
-                html = '<img src onerror="{}">'.format(js)
-                div = Div(text=html)
-                st.bokeh_chart(div) 
-                return sp
+        return sp
 
 
 #初期表示
